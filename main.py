ce1324d0a2da904e9a0cdd090587d14180df31f0
lucky <muraha.2000.labac@gmail.com>
2022-10-07 10:45:56 +0900

    data load before (editing)

--- main.py
+++ main.py
@@ -25,12 +25,11 @@ import models.models as models
 import utils.confusion as confusion
 import utils.my_trainer as trainer
 import utils.train_result as train_result
-from datasets.dataset import load_data
+from datasets.dataset import CLASS_MAP, load_data
 from utils.data_class import BrainDataset
 
-CLASS_MAP = {"CN": 0, "AD": 1}
+#CLASS_MAP = {"CN": 0, "AD": 1}
 SEED_VALUE = 82
-
 
 def parser():
     parser = argparse.ArgumentParser(description="example")
@@ -54,8 +53,14 @@ def fix_seed(seed):
     return
 fix_seed(0)
 
-CLASS_MAP = {"CN": 0, "AD": 1}
-SEED_VALUE = 0
+
+
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2 ** 32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
 
 
 
@@ -84,15 +89,62 @@ class ImageTransformio():
 
 def load_dataloader(n_train_rate, batch_size):
     data = load_data(kinds=["ADNI2", "ADNI2-2"], classes=["CN", "AD"], unique=False, blacklist=True)
-  # data = load_data(kinds=["ADNI2","ADNI2-2"], classes=["CN", "AD"], unique=True, blacklist=True)
-  # data = dataset.load_data(kinds=kinds,classes=classes,unique=False)
+
     pids = []
+    voxels = np.zeros((len(data), 80, 96, 80))
+    labels = np.zeros(len(data))
+    for i in tqdm(range(len(data))):
+        pids.append(data[i]["pid"])
+        voxels[i] = data[i]["voxel"]
+        labels[i] = CLASS_MAP[data[i]["label"]]
+    pids = np.array(pids)
+
+
+    gss = GroupShuffleSplit(test_size=0.2, random_state=42)
+    tid, vid = list(gss.split(voxels, groups=pids))[0]
+    train_voxels = voxels[tid]
+    val_voxels = voxels[vid]
+    train_labels = labels[tid]
+    val_labels = labels[vid]
+
+    train_dataset = BrainDataset(train_voxels, train_labels)
+    val_dataset = BrainDataset(val_voxels, val_labels)
+
+
+
     for i in range(len(data)):
         pids.append(data[i]["pid"])
     gss = GroupShuffleSplit(test_size=1 - n_train_rate, random_state=SEED_VALUE)
     train_idx, val_idx = list(gss.split(data, groups=pids))[0]
     train_data = data[train_idx]
     val_data = data[val_idx]
+
+    g = torch.Generator()
+    g.manual_seed(0)
+
+    #num_workers = 2
+    batch_size = 16
+    train_dataset = BrainDataset(train_voxels, train_labels)
+    val_dataset = BrainDataset(val_voxels, val_labels)
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        num_workers=os.cpu_count(),
+        pin_memory=True,
+        shuffle=True,
+        worker_init_fn=seed_worker,
+        generator=g
+    )
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        num_workers=os.cpu_count(),
+        pin_memory=True,
+        shuffle=False,
+        worker_init_fn=seed_worker,
+        generator=g
+    )
+
 
     #train_datadict, val_datadict = train_test_split(dataset, test_size=1-n_train_rate, shuffle=True, random_state=SEED_VALUE)
     transform = ImageTransformio()
@@ -104,6 +156,20 @@ def load_dataloader(n_train_rate, batch_size):
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, num_workers=os.cpu_count(), pin_memory=True, shuffle=False)
 
     return train_dataloader, val_dataloader
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
@@ -134,11 +200,12 @@ def main():
         f.write("{}".format(args))
 
 
-
     os.environ["CUDA_VISIBLE_DEVICES"] = "6"   #  os.environ["CUDA_VISIBLE_DEVICES"]="4,5,6,7"
     device = torch.device("cuda" if torch.cuda.is_available() and True else "cpu")
     print("device:", device)
 
+
+#   ここで データをロードする .
     train_loader, val_loader = load_dataloader(args.n_train, args.batch_size)
     # loadnet or train
     if args.train_or_loadnet == "loadnet":
