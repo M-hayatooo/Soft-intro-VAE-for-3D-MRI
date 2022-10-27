06f1b43b7eab0a59abb9de640a72f9401121b5f6
lucky <muraha.2000.labac@gmail.com>
2022-10-27 16:48:44 +0900

    result write function added

--- utils/train_result.py
+++ utils/train_result.py
@@ -46,6 +46,38 @@ def result_ae(train_loss, val_loss, path="."):
     fig1.savefig(path + "/img/loss.png")
 
 
+def result_S_IntroVAE(train_lossE, train_lossD, val_lossE, val_lossD, path="."):
+    os.makedirs(path + "/img", exist_ok=True)
+    epoch = len(train_lossE)
+    plt.rcParams["font.size"] = 18
+    fig1, ax1 = plt.subplots(figsize=(10, 10))
+    ax1.plot(range(1, epoch + 1), train_lossE, label="train_lossE")
+    ax1.plot(range(1, epoch + 1), train_lossD, label="train_lossD")
+    ax1.plot(range(1, epoch + 1), val_lossE, label="val_lossE")
+    ax1.plot(range(1, epoch + 1), val_lossD, label="val_lossD")
+    ax1.set_title("loss")
+    ax1.set_xlabel("epoch")
+    ax1.set_ylabel("loss")
+    ax1.legend()
+    fig1.savefig(path + "/img/loss.png")
+
+
+def result_rec_kls_loss(kls_real, kls_fake, kls_rec, rec_errs, path="."):
+    os.makedirs(path + "/img", exist_ok=True)
+    epoch = len(kls_real)
+    plt.rcParams["font.size"] = 18
+    fig1, ax1 = plt.subplots(figsize=(10, 10))
+    ax1.plot(range(1, epoch + 1), kls_real, label="kls_real")
+    ax1.plot(range(1, epoch + 1), kls_fake, label="kls_fake")
+    ax1.plot(range(1, epoch + 1), kls_rec, label="kls_rec")
+    ax1.plot(range(1, epoch + 1), rec_errs, label="rec_errs")
+    ax1.set_title("kls_loss")
+    ax1.set_xlabel("epoch")
+    ax1.set_ylabel("kls_loss")
+    ax1.legend()
+    fig1.savefig(path + "/img/kls_loss.png")
+
+
 
 def read_csv(path):
     df = pd.read_csv(path)
